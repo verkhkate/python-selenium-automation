8d92690dac7d346afaa2640c53b07553d088946b
Kate V <verkh.kate@gmail.com>
2022-05-01 15:00:50 -0500

    Inserted print statements into the function body, which is the correct way

--- Lessons/Lesson3/L3_HW_Codewars_kata.py
+++ Lessons/Lesson3/L3_HW_Codewars_kata.py
@@ -7,12 +7,11 @@
 
 def convert_fn_and_ln_to_initials(fn, ln):
     result_converting_fn_and_ln_to_initials = (fn[0] + "." + ln[0]).upper()
+    print(f"\nConverting your full name into initials... \n{first_name} {last_name} => {result_converting_fn_and_ln_to_initials}")
+    print("\nThanks for participating! You've got a great name, by the way ;)")
     return result_converting_fn_and_ln_to_initials
 
 first_name = input("\nPlease enter your first name: ")
 last_name = input("Please enter your last name: ")
 
 result_converting_fn_and_ln_to_initials = convert_fn_and_ln_to_initials(first_name, last_name)
-
-print(f"\nConverting your full name into initials... \n{first_name} {last_name} => {result_converting_fn_and_ln_to_initials}")
-print("\nThanks for participating! You've got a great name, by the way ;)")
